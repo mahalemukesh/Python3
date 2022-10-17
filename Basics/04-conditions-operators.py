ec8611b020b2fbc891851a2932e3e2b6acf430b7
mahalemukesh <84039982+mahalemukesh@users.noreply.github.com>
2022-10-18 01:16:59 +0530

    Create 04-conditions-operators.py

---
+++ Basics/04-conditions-operators.py
@@ -0,0 +1,21 @@
+# Conditinal operators
+# > greater than
+# < less than
+# == equal to (It will be always difine with "==" as "=" sigle sign used to define variable ex."name = 'mark'"
+
+print(2 < 3)
+print(2 > 3)
+print('Hi' == 'Hello')
+print('Hello' != 'hello')
+
+## Sample output
+>>> print(2 < 3)
+True
+>>> print(2 > 3)
+False
+>>> print('Hi' == 'Hello')
+False
+>>> print('Hello' != 'hello')
+True
+>>> 
+
