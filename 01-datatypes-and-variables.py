eeaf239265a77f338be33804146b678bbdac76d6
mahalemukesh <84039982+mahalemukesh@users.noreply.github.com>
2022-10-18 00:28:14 +0530

    Create 01-datatypes-and-variables.py

---
+++ 01-datatypes-and-variables.py
@@ -0,0 +1,29 @@
+#### Data types
+# Interger (any whole number including - numbers ex. 1,2 3, .... -1,-2,-3)
+int 1, 3, 8, 10,453
+
+# Float (Any decimal number)
+float 1.29, 2.89, 3.67
+
+#Strings (Anything in between quotes single ('') or double (""))
+str 'Mukesh' "Gauri" "'Siya'" "123" '586' '1'
+
+#Booline (True or False)
+bool True, False
+
+
+#### Variables
+#define "name" as variable
+name = "mukesh"
+#- To run the code
+print(name)
+
+#Define variable with datatype int
+age = 21
+print(age)
+
+# Variables cannot start with -, _ ,number or any syntax like *, $, %, # etc
+# It will be case sensitive, below are the exaples
+NAME = "Mark"
+name = "Peter"
+Name = "Bea"
