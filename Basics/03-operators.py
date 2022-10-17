c03588e71037b3fffc7aa074829a7c6f0f04e91e
mahalemukesh <84039982+mahalemukesh@users.noreply.github.com>
2022-10-18 01:00:39 +0530

    Create 03-operators.py

---
+++ Basics/03-operators.py
@@ -0,0 +1,32 @@
+### Mathematical operatos
+# + Addition
+# - Subsctraction
+# * Multiplecation
+# / Division
+# ** Exponential
+# // Modulus (reminer after division)
+
+num1 = 10
+num2 = 5
+print(num1 + num2)
+
+## Sample output
+#>>> num1 = 10
+#>>> num2 = 5
+#>>> print(num1 + num2)
+#15
+
+#>>> print(num1 - num2)
+#5
+
+#>>> print(num1 * num2)
+#50
+
+#>>> print(num1 / num2)
+#2.0
+
+#>>> print(num1 // num2)
+#2
+
+#>>> print(num1 ** num2)
+#100000
