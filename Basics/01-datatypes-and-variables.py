9e73252c562f947cdb8de13b480049f6e382e368
mahalemukesh <84039982+mahalemukesh@users.noreply.github.com>
2022-10-18 00:47:30 +0530

    Create 01-datatypes-and-variables.py

---
+++ Basics/01-datatypes-and-variables.py
@@ -0,0 +1,44 @@
+#### Data types
+# Interger (any whole number including - numbers ex. 1,2 3, .... -1,-2,-3)
+int 1, 3, 8, 10,453
+
+# Float (Any decimal number)
+float 1.29, 2.89, 3.67
+
+#Strings (Anything in between quotes single ('') or double (""))
+str 'Mukesh' "Gauri" "'Siya'" "123" '586' '1'
+
+#Booline (True or False)
+bool True, False
+
+
+#### Variables
+#define "name" as variable
+name = "mukesh"
+#- To run the code
+print(name)
+
+### Sample output
+##Python 3.10.6 (main, Aug 10 2022, 11:40:04) [GCC 11.3.0] on linux
+##Type "help", "copyright", "credits" or "license" for more information.
+#>>> name = "Mukesh"
+#>>> print(name)
+#Mukesh
+#>>> 
+###
+
+#Define variable with datatype int
+age = 21
+print(age)
+
+# Variables cannot start with -, _ ,number or any syntax like *, $, %, # etc
+# It will be case sensitive, below are the exaples
+NAME = "Mark"
+name = "Peter"
+Name = "Bea"
+
+### Sample output for using * in variable name
+#>>> *name = "mark"
+#  File "<stdin>", line 1
+#SyntaxError: starred assignment target must be in a list or tuple
+#>>> 
