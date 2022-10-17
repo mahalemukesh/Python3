573e060be76a0f71ae6a4a7f1d88a91edf4f6fef
mahalemukesh <84039982+mahalemukesh@users.noreply.github.com>
2022-10-18 01:17:41 +0530

    Update 03-operators.py

--- Basics/03-operators.py
+++ Basics/03-operators.py
@@ -11,22 +11,22 @@ num2 = 5
 print(num1 + num2)
 
 ## Sample output
-#>>> num1 = 10
-#>>> num2 = 5
-#>>> print(num1 + num2)
-#15
+>>> num1 = 10
+>>> num2 = 5
+>>> print(num1 + num2)
+15
 
-#>>> print(num1 - num2)
-#5
+>>> print(num1 - num2)
+5
 
-#>>> print(num1 * num2)
-#50
+>>> print(num1 * num2)
+50
 
-#>>> print(num1 / num2)
-#2.0
+>>> print(num1 / num2)
+2.0
 
-#>>> print(num1 // num2)
-#2
+>>> print(num1 // num2)
+2
 
-#>>> print(num1 ** num2)
-#100000
+>>> print(num1 ** num2)
+100000
