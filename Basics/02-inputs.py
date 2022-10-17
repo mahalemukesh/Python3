85c3c3d410b79acfe32af18981e0746df9b59d5f
mahalemukesh <84039982+mahalemukesh@users.noreply.github.com>
2022-10-18 01:18:09 +0530

    Update 02-inputs.py

--- Basics/02-inputs.py
+++ Basics/02-inputs.py
@@ -4,7 +4,7 @@ print('Hello ,', name)
 
 
 ## Sample output
-#$ python3.10 inputs.py 
-#what is your name? <------ Questioned from code
-#Mukesh         <---------- Enter by user
-#Hello, Mukesh  <---------- Response from code
+$ python3.10 inputs.py 
+what is your name? <------ Questioned from code
+Mukesh         <---------- Enter by user
+Hello, Mukesh  <---------- Response from code
