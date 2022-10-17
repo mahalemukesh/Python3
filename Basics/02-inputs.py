f09af0aafeaf18a4f2971757da6b99260eac4906
mahalemukesh <84039982+mahalemukesh@users.noreply.github.com>
2022-10-18 00:45:24 +0530

    Create 02-inputs.py

---
+++ Basics/02-inputs.py
@@ -0,0 +1,10 @@
+print('What is your name?')
+name = input()
+print('Hello ,', name)
+
+
+## Sample output
+#$ python3.10 inputs.py 
+#what is your name? <------ Questioned from code
+#Mukesh         <---------- Enter by user
+#Hello, Mukesh  <---------- Response from code
