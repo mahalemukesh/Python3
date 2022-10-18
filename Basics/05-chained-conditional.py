579862a406ce7e49d8420981a3c7d2602f0bc58c
mahalemukesh <84039982+mahalemukesh@users.noreply.github.com>
2022-10-18 09:58:47 +0530

    Create 05-chained-conditional.py

---
+++ Basics/05-chained-conditional.py
@@ -0,0 +1,15 @@
+x = 4
+y = 7
+
+if y == x and x + y == 11:
+  print('It works !!!')
+else:
+  print(':(')
+  
+  
+##Sample output
+$ python3.10 chained.py     <-------- With 'and' conditional
+:( 
+
+$ python3.10 chained.py     <-------- With 'or' conditional
+It works !!!
