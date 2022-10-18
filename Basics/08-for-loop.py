55013bf1d6e415b7efe62c33e89c16f8d66827e4
mahalemukesh <84039982+mahalemukesh@users.noreply.github.com>
2022-10-18 10:16:55 +0530

    Create 08-for-loop.py

---
+++ Basics/08-for-loop.py
@@ -0,0 +1,28 @@
+for x in range(0, 10, 1): #start, stop, step
+  print(x)
+
+  
+#Above range can defined as (0, 10, 1/2/5), or (0, 10) or (10)  
+#Sample output
+$ python3.10 for.py                  <--- Step = 1
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+$ python3.10 for.py                 <--- Step = 2
+0
+2
+4
+6
+8
+
+$ python3.10 for.py                 <--- Step = 5 
+0
+5
