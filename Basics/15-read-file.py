7b1466dd770e59481137d681e3f2791ea13f514f
mahalemukesh <84039982+mahalemukesh@users.noreply.github.com>
2022-10-18 13:25:38 +0530

    Create 15-read-file.py

---
+++ Basics/15-read-file.py
@@ -0,0 +1,20 @@
+file = open('text.txt', 'r')
+f = file.readlines()
+
+print(f)
+
+##Sample output
+$ python3.10 read_file.py 
+['adsd\n', 'mbaslnfslk\n', 'bfcsds\n', 'nsdknsdkc\n', 'ndslkcdksnc\n', 'ncslkncsn\n', 'knjbos\n', 'nbsdkc\n']
+
+
+## To remove all \n, try with below code
+newList = []
+for line in f:
+    newList.append(line.strip())
+
+print(newList)
+
+##Sample output
+$ python3.10 read_file.py 
+['adsd', 'mbaslnfslk', 'bfcsds', 'nsdknsdkc', 'ndslkcdksnc', 'ncslkncsn', 'knjbos', 'nbsdkc']
